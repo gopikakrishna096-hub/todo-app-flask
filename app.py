30752f9c5ca16f1d06a345cbaa9ace986460c304
gopikakrishna096-hub <gopikakrishna096@gmail.com>
2026-05-22 21:43:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,5 +20,7 @@ def delete_task(task_id):
         tasks.pop(task_id)
     return redirect('/')
 
+import os
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
